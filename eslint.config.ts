209fe3df574c6d9706401607c62ddcdeb1a219a7
Android Studio <devandroid@ort>
2025-10-23 10:23:28 -0300

    extras

--- eslint.config.ts
+++ eslint.config.ts
@@ -3,7 +3,6 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from '@typescript-eslint/eslint-plugin'
-import tsparser from '@typescript-eslint/parser'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
@@ -31,7 +30,6 @@ export default defineConfig([
   {
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
-      parser: tsparser,
       ecmaVersion: 2020,
       globals: globals.browser,
       parserOptions: {
